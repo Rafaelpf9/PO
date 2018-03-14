ed37a86015b630b0d06e3d118a3521d7d038ffaa
Rafaelpf9 <rafaelpf9@gmail.com>
2018-03-14 19:44:01 -0300

    Create bucketsort.py

---
+++ bucketsort.py
@@ -0,0 +1,86 @@
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+from timeit import timeit
+from random import randint
+import numpy as np
+import scipy.interpolate as interpolate
+import math
+
+def geraLista(tam):
+    lista = []
+    for i in range(tam):
+        n = randint(1,1*tam)
+        if n not in lista: lista.append(n)
+    return lista
+
+def geraListaDecrescente(tam):
+    lista = []
+    x = tam
+    for i in range(tam):
+        lista.append(x)
+        x -= 1
+    return lista
+
+def hashing(lists):
+    m = lists[0]
+    for i in range(1, len(lists)):
+        if (m < lists[i]):
+            m = lists[i]
+    result = [m, int(math.sqrt(len(lists)))]
+    return result
+
+
+def re_hashing(i, code):
+  return int(i / code[0] * (code[1] - 1))
+  
+def insertionsort(lists):
+    for i in range(1, len(lists)):
+        temp = lists[i]
+        j = i
+        while j > 0 and temp < lists[j - 1]:
+            lists[j] = lists[j - 1]
+            j -= 1
+        lists[j] = temp
+
+def desenhaGrafico(x,y,w,xl = "Nº de Elementos", yl = "Tempo(s)"):
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111)
+    xnew = np.linspace(min(x), max(x), 10 * (max(x) - min(x)))
+    a, b, c = interpolate.splrep(x, y, s=0, k=2)
+    suave = interpolate.BSpline(a, b, c, extrapolate=False)
+    d, e, f = interpolate.splrep(x, w, s=0, k=2)
+    suave2 = interpolate.BSpline(d, e, f, extrapolate=False)
+    plt.plot(xnew, suave2(xnew), label="Decrescente")
+    plt.plot(xnew, suave(xnew), label="Aleatória")
+    ax.legend(bbox_to_anchor=(1, 1),bbox_transform=plt.gcf().transFigure)
+    plt.ylabel(yl)
+    plt.xlabel(xl)
+    plt.title('bucketsort', fontsize=18)
+    fig.savefig('bucketsort.png')
+
+
+def bucketsort(lists):
+    code = hashing(lists)
+    buckets = [list() for _ in range(code[1])]
+    for i in lists:
+        x = re_hashing(i, code)
+        buck = buckets[x]
+        buck.append(i)
+    for bucket in buckets:
+        insertionsort(bucket)
+
+    ndx = 0
+    for b in range(len(buckets)):
+        for v in buckets[b]:
+            lists[ndx] = v
+            ndx += 1
+
+
+aleatorio = []
+decrescente = []
+x = [1000, 3000, 6000, 9000, 12000, 15000, 18000, 21000, 24000]
+for i in x:
+    aleatorio.append(timeit("bucketsort({})".format(geraLista(i)), setup="from __main__ import bucketsort", number=1))
+    decrescente.append(timeit("bucketsort({})".format(geraListaDecrescente(i)), setup="from __main__ import bucketsort", number=1))
+desenhaGrafico(x, aleatorio, decrescente)
